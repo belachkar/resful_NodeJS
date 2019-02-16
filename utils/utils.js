bc2f99d23b69009369d65935c88429bab3449198
Ali <34995109+belachkar@users.noreply.github.com>
2019-02-16 02:19:55 +0100

    Add Workers Service

--- utils/utils.js
+++ utils/utils.js
@@ -9,15 +9,24 @@ utils.isBoolean = (data) => {
 utils.isNumber = (data) => {
   return typeof (data) == 'number';
 };
+utils.isArray = (data) => {
+  return Array.isArray(data);
+};
+utils.isObject = (data) => {
+  return typeof(data) == 'object' && data !== null;
+};
 utils.isEmpty = (data) => {
   data = utils.isString(data) ? data.trim() : data;
   return !(data.length > 0);
 };
-utils.hasLength = (data, length) => {
-  return data.length === length;
-};
 utils.isStringNotEmpty = (data) => {
   return utils.isString(data) && !(utils.isEmpty(data));
+};
+utils.isArrayNotEmpty = (data) => {
+  return utils.isArray(data) && !(utils.isEmpty(data));
+};
+utils.isObjectNotEmpty = (data) => {
+  return utils.isObject(data) && data !== {};
 };
 utils.isNumberPositif = (data) => {
   return utils.isNumber(data) && data > 0;
@@ -31,15 +40,14 @@ utils.isNumberNatural = (data) => {
 utils.isStringWithLength = (data, length) => {
   return utils.isString(data) && utils.hasLength(data.trim(), length);
 };
-
 utils.isStringLengthBetweenStrict = (data, minLength, maxLength) => {
   return utils.isString(data) && data.trim().length > minLength && data.trim().length < maxLength;
 };
 utils.isBooleanTrue = (data) => {
   return utils.isBoolean(data) && data == true;
 };
-utils.isArrayNotEmpty = (data) => {
-  return Array.isArray(data) && data.length > 0;
+utils.hasLength = (data, length) => {
+  return data.length === length;
 };
 
 module.exports = utils;
